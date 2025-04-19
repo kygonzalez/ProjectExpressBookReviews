f22caf1e8236baaff9107defc1c0ce3fce4822ae
Yuliana Gonzalez <yulianagonzalezz@hotmail.com>
2025-04-19 15:49:58 -0500

    feat: create method to register new user and its validations
    - task #6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,23 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //validate if username already exists
+  const userExists = users.some(user => user.username === username);
+  if (userExists) {
+    return res.status(400).json({message: "User already exists"});
+  }
+
+  //validate if username and password are provided
+  if (!username || !password) {
+    return res.status(400).json({message: "Username and password are required"});
+  }
+
+  users.push({username: username, password: password});
+  return res.status(200).json({message: "User registered successfully"});
 });
 
 // Get the book list available in the shop
