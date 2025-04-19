a9b88fa1a4cf2d6c012e6a509a4363e49ea312cd
Yuliana Gonzalez <yulianagonzalezz@hotmail.com>
2025-04-19 18:07:14 -0500

    feat: convert general method to use promises and asyn/await
    - task # 11, 12 & 13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,40 +47,78 @@ public_users.get('/', async function (req, res) {
 });
 
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+// Get book details based on ISBN (async)
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
-  const book = books[isbn]; // Find the book in the books object using the key
+  try {
+    const getBook = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const book = books[isbn]; 
+        if (book) {
+          resolve(book);
+        } else {
+          reject(new Error("Book not found for ISBN: " + isbn)); 
+        }
+      }, 50);
+    });
 
-  if (book) {
-    res.status(200).json(book);
-  } else {
-    res.status(404).json({ message: "Book not found for ISBN: " + isbn });
+    const fetchedBook = await getBook;
+    res.status(200).json(fetchedBook);
+
+  } catch (error) {
+        console.error("Error fetching book by ISBN:", error);
+        res.status(500).json({ message: "Failed to retrieve book details" });
   }
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   const author = req.params.author; 
-  const booksByAuthor = Object.values(books).filter(book => book.author === author); // Filter books by author
+  try {
+    const getBookByAuthor = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const booksByAuthor = Object.values(books).filter(book => book.author === author); // Filter books by author
+        if (booksByAuthor.length > 0) {
+          resolve(booksByAuthor);
+        } else {
+          reject(new Error("Book not found for author: " + author)); 
+        }
+      }, 50);
+    });
 
-  if (booksByAuthor.length > 0) {
-    res.status(200).json(booksByAuthor);  
-  } else {
-    res.status(404).json({ message: "No books found for author: " + author });
+    const fetchedBookByAuthor = await getBookByAuthor;
+    res.status(200).json(fetchedBookByAuthor);
+
+  } catch (error) {
+        console.error("Error fetching books by author: " + author, error);
+        res.status(500).json({ message: "Failed to retrieve book details" });
   }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title',async function (req, res) {
   const title = req.params.title; 
-  const booksByTitle = Object.values(books).filter(book => book.title === title); // Filter books by title
+  try {
+    const getBookByTitle = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const booksByTitle = Object.values(books).filter(book => book.title === title); // Filter books by title
+        if (booksByTitle.length > 0) {
+          resolve(booksByTitle);
+        } else {
+          reject(new Error("Book not found for title: " + title)); 
+        }
+      }, 50);
+    });
 
-  if (booksByTitle.length > 0) {
-    res.status(200).json(booksByTitle);
-  } else {
-    res.status(404).json({ message: "No books found for title: " + title });
+    const fetchedBookByTitle = await getBookByTitle;
+    res.status(200).json(fetchedBookByTitle);
+
+  } catch (error) {
+        console.error("Error fetching books by title: " + title, error);
+        res.status(500).json({ message: "Failed to retrieve book details" });
   }
+
+
 });
 
 //  Get book review
